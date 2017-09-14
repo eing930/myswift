fe8d37cf525a2e33cb740eeadf20f2882163c36c
yvonne <eing930@gmail.com>
2017-09-14 11:17:32 +0800

    56

---
+++ Myswift08/Myswift08/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Myswift08
+//
+//  Created by user20 on 2017/9/14.
+//  Copyright © 2017年 Yvonne Big. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
